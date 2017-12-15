994d00a5823f72975f1cb932f8c5bc4ce61609ef
dejanle <dejnik978@gmail.com>
2017-12-15 02:17:19 +0100

    add bootstrap col-md-3 styel

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,28 +5,29 @@ class Gallery extends Component {
   render() {
     return (
       <div className="App">
-         
+         <div className="container">
+        			<div className="row">
         {
         	this.props.items.map((item, index) => {
         		let {title, imageLinks, infoLink} = item.volumeInfo;
         		return(
-        		<div className="container">
-        			<div className="row">
-							<div className="col-md-3">
-									<div className="card">
-									 <img className='card-img-top' src={imageLinks !== undefined ? imageLinks.thumbnail : ''} alt="book image" /> 
-									  <div className="card-body">
-									    <h4 className="card-title"><a key={index} className="book" href={infoLink} target="_blank">{title}</a></h4>
-									    
-									  </div>
-									</div> 	 
-								</div>
-							</div>	
- 
-        	 	</div>
-        		)
-        	})
-        }
+        		
+									<div className="col-md-3">
+												<div className="card">
+												 		<img className='card-img-top thumbnail' src={imageLinks !== undefined ? imageLinks.thumbnail : ''} alt="book image" /> 
+													  <div className="card-body">
+													    <h4 className="card-title"><a key={index} className="book" href={infoLink} target="_blank">{title}</a></h4>
+													    
+													  </div>
+												</div> 	 
+										</div>
+
+		        		)
+		        	})
+        		}
+
+					 	</div>	 
+         </div>	
       </div>
     ) 
   }
